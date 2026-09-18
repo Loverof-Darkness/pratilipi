e3043423a174dbe76430f49100e48038f57d8a8a
LoverofDarkness <loverofdarkness@mail2indian.com>
2026-09-18 22:53:07 +0530

    feat: make shared links public

--- functions/u/_middleware.js
+++ functions/u/_middleware.js
@@ -1,7 +1,3 @@
-import { error, getAuthSession } from '../_lib.js';
-
-export async function onRequest({ request, env, next }) {
-  const session = await getAuthSession(request, env);
-  if (!session) return error('Authentication required.', 401);
+export async function onRequest({ next }) {
   return next();
 }
